78090e1ed845461862a15870fba9c965ac6133cb
dera- <alchemy.and.fairy@gmail.com>
2023-11-06 22:56:47 +0900

    自動更新とexe生成ができるように

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
 // Modules to control application life and create native browser window
-const { app, BrowserWindow } = require('electron')
-const path = require('node:path')
+const { app, BrowserWindow } = require('electron');
+const path = require('node:path');
+const { updateElectronApp } = require('update-electron-app');
+
+updateElectronApp();
 
 function createWindow () {
   // Create the browser window.
