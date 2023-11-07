3aea30c0c8bd275662d704dc9844d1e382b56f25
dera- <alchemy.and.fairy@gmail.com>
2023-11-08 02:29:48 +0900

    v1.0.2

--- main.js
+++ main.js
@@ -1,9 +1,13 @@
 // Modules to control application life and create native browser window
 const { app, BrowserWindow } = require('electron');
 const path = require('node:path');
-const { updateElectronApp } = require('update-electron-app');
+const { autoUpdater } = require('electron-updater');
+const log = require('electron-log');
 
-updateElectronApp();
+// アップデートに関する情報をログファイルへ出力
+autoUpdater.logger = log;
+autoUpdater.logger.transports.file.level = 'info';
+
 
 function createWindow () {
   // Create the browser window.
@@ -26,7 +30,10 @@ function createWindow () {
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
 app.whenReady().then(() => {
-  createWindow()
+  createWindow();
+
+  // アップデートをチェック
+  autoUpdater.checkForUpdatesAndNotify();
 
   app.on('activate', function () {
     // On macOS it's common to re-create a window in the app when the
@@ -44,3 +51,39 @@ app.on('window-all-closed', function () {
 
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and require them here.
+
+//-------------------------------------------
+// 自動アップデート関連のイベント処理
+//-------------------------------------------
+// アップデートをチェック開始
+autoUpdater.on('checking-for-update', () => {
+  log.info(process.pid, 'checking-for-update...');
+})
+// アップデートが見つかった
+autoUpdater.on('update-available', (ev, info) => {
+  log.info(process.pid, 'Update available.');
+})
+// アップデートがなかった（最新版だった）
+autoUpdater.on('update-not-available', (ev, info) => {
+  log.info(process.pid, 'Update not available.');
+})
+// アップデートのダウンロードが完了
+autoUpdater.on('update-downloaded', (info) => {
+  const dialogOpts = {
+    type: 'info',
+    buttons: ['更新して再起動', 'あとで'],
+    message: 'アップデート',
+    detail: '新しいバージョンをダウンロードしました。再起動して更新を適用しますか？'
+  }
+
+  // ダイアログを表示しすぐに再起動するか確認
+  dialog.showMessageBox(mainWin, dialogOpts).then((returnValue) => {
+    if (returnValue.response === 0){
+      autoUpdater.quitAndInstall()
+    }
+  })
+});
+// エラーが発生
+autoUpdater.on('error', (err) => {
+  log.error(process.pid, err);
+})
